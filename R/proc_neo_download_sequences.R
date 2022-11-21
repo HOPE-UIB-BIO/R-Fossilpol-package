713d42db539bd39c3280bcc1222dd3439068316c
Ondřej Mottl <60345393+OndrejMottl@users.noreply.github.com>
2022-11-21 14:51:32 +0100

    Neotoma - download sequences (#18)

--- R/proc_neo_download_sequences.R
+++ R/proc_neo_download_sequences.R
@@ -1,108 +1,233 @@
 #' @title Download all sequences from Neotoma
 #' @param allds Data.frame with all dataset IDs
+#' @param dir Path to the data storage folder
 #' @param n_tries Number of tries to download each dataset
+#' @return List with all downloaded sequences
 #' @export
-proc_neo_download_sequences <-
-  function(allds, n_tries = 10) {
-    RUtilpol::check_class("allds", "data.frame")
+proc_neo_download_sequences <- function(allds,
+                                        dir,
+                                        n_tries = 10) {
+  RUtilpol::check_class("allds", "data.frame")
 
-    RUtilpol::check_col_names("allds", "dsid")
+  RUtilpol::check_col_names("allds", "dsid")
 
-    RUtilpol::check_class("n_tries", "numeric")
+  RUtilpol::check_class("n_tries", "numeric")
 
-    # make a vector with all se dataset_ids
-    ds_vector <- 
-      allds  %>% 
-      purrr::pluck("dsid")  %>% 
-      sort()
+  # helper functions
+  get_clean_name <- function(data_source) {
 
-    rawdownload <- 
-      "https://api.neotomadb.org/v2.0/data/downloads"
-
-    result_list <-
-      purrr::map(
-        .x = seq_along(ds_vector),
-        .f = ~ {
-
-          # repeat for 'n_tries' or until successfully download the sequence
-          for (i in 1:n_tries) {
-            current_frame <- sys.nframe()
-            current_env <- sys.frame(which = current_frame)
-
-            # output progress
-            cat(paste0(.x, " in ", length(ds_vector)))
-
-            # download the sequence
-            res <-
-              httr::GET(paste0(rawdownload, "/", ds_vector[.x]))
-
-            # if it was successful download
-            if (
-              res$status_code == 200 # NOTE: status_code 200 = success
-            ) {
-
-              # extract the data
-              output <-
-                httr::content(res)$data[[1]]
-
-              # output 'success'
-              cat(" - success", "\n")
-
-              # break from loop
-              break
-            } else {
-
-              # save output as nothing
-              output <- NULL
-
-              # output 'success'
-              cat(" - fail", "\n")
-            }
-
-            # output NULL for the last try
-            if (
-              i == n_tries
-            ) {
-              return(output)
-            }
-
-            # delete the result and output
-            rm(res, output, envir = current_env)
-          }
-
-          return(output)
-        }
-      ) %>%
-      purrr::set_names( # add the names of sequences
-        nm = ds_vector
+    # detect dates
+    data_source_date_apend <-
+      paste0(
+        ".",
+        RUtilpol:::get_date_from_name(data_source),
+        ".*"
       )
 
+    # remove dates
+    data_source_striped <-
+      stringr::str_replace(
+        string = data_source,
+        pattern = data_source_date_apend,
+        ""
+      )
 
-    # extract the sequences which were not successfully downloaded
-    cannot_download <-
-      allds %>%
-      dplyr::filter(purrr::map_lgl(result_list, is.null)) %>%
-      purrr::pluck("dsid")
+    return(data_source_striped)
+  }
+
+  get_missing_names <- function(path_to_folder, name_vector) {
+
+    # pre-alocate that all sequences are missing
+    ds_absent <- name_vector
+
+    # check which sequences are present
+    ds_present <-
+      list.files(
+        path_to_folder
+      )
 
     if (
-      length(cannot_download) == 0
+      length(ds_present) > 0
     ) {
-      usethis::ui_done("All selected sequences were downloaded")
-    } else {
-      usethis::ui_oops(
-        paste(
-          length(cannot_download), "out of", nrow(allds),
-          "sequences were NOT downloaded.", "\n",
-          "Specifically, dataset IDs:",
-          RUtilpol::paste_as_vector(cannot_download)
-        )
-      )
+      ds_present_striped <-
+        get_clean_name(ds_present)
+
+      # select data NOT downloaded
+      ds_absent <-
+        name_vector[!name_vector %in% ds_present_striped]
     }
 
-    # save the final list as 'neotoma_download' object filtering out unsuccessfull
-    #   sequences
-    neotoma_download <-
-      result_list[!purrr::map_lgl(result_list, is.null)]
-
-    return(neotoma_download)
+    return(ds_absent)
   }
+
+  # make a vector with all se dataset_ids
+  ds_vector <-
+    allds %>%
+    purrr::pluck("dsid") %>%
+    sort() %>%
+    as.character()
+
+  dir <- RUtilpol::add_slash_to_path(dir)
+
+  path_to_indiv_folder <-
+    paste0(dir, "individual/")
+
+  if (
+    isFALSE(
+      any(
+        "individual" %in% list.files(dir)
+      )
+    )
+  ) {
+    dir.create(
+      path_to_indiv_folder
+    )
+  }
+
+  # get all missing names
+  ds_absent <-
+    get_missing_names(
+      path_to_folder = path_to_indiv_folder,
+      name_vector = ds_vector
+    )
+
+  n_seq_to_download <- length(ds_absent)
+
+  # if all sequences are downloaded
+  if (
+    n_seq_to_download < 1
+  ) {
+    usethis::ui_done("All selected sequences are downloaded")
+    return()
+  }
+
+  RUtilpol::output_comment(
+    paste(
+      n_seq_to_download,
+      "sequence(s) will be downloaded"
+    )
+  )
+
+  # path to the Neotoma API
+  rawdownload <-
+    "https://api.neotomadb.org/v2.0/data/downloads"
+
+
+  # download all sequences individually
+  purrr::walk(
+    .x = 1:n_seq_to_download,
+    .f = ~ {
+      sel_seq_name <- ds_absent[.x]
+
+      # output progress
+      cat(
+        paste(
+          "downloading", .x, "of", n_seq_to_download
+        )
+      )
+
+      # repeat for 'n_tries' or until successfully download the sequence
+      for (i in 1:n_tries) {
+        current_frame <- sys.nframe()
+        current_env <- sys.frame(which = current_frame)
+
+        # download the sequence
+        res <-
+          httr::GET(
+            paste0(rawdownload, "/", sel_seq_name)
+          )
+
+        # if it was successful download
+        if (
+          res$status_code == 200 # NOTE: status_code 200 = success
+        ) {
+
+          # extract the data
+          output <-
+            httr::content(res)$data[[1]]
+
+          # output 'success'
+          cat(" - success", "\n")
+
+          RUtilpol::save_latest_file(
+            object_to_save = output,
+            file_name = sel_seq_name,
+            dir = path_to_indiv_folder,
+            prefered_format = "rds",
+            use_sha = TRUE,
+            verbose = FALSE
+          )
+
+          # break from loop
+          break
+        } else {
+
+          # save output as nothing
+          output <- NULL
+
+          # output 'success'
+          cat(" - fail", "\n")
+        }
+
+        # delete the result and output
+        rm(res, output, envir = current_env)
+      }
+    }
+  )
+
+  # check again if all are downloaded
+  # extract the sequences which were not successfully downloaded
+  cannot_download <-
+    get_missing_names(
+      path_to_folder = path_to_indiv_folder,
+      name_vector = ds_vector
+    )
+
+  if (
+    length(cannot_download) == 0
+  ) {
+    usethis::ui_done("All selected sequences were downloaded")
+  } else {
+    usethis::ui_oops(
+      paste(
+        length(cannot_download), "out of", nrow(allds),
+        "sequences were NOT downloaded.", "\n",
+        "Specifically, dataset IDs:",
+        RUtilpol::paste_as_vector(cannot_download)
+      )
+    )
+  }
+
+  ds_present <-
+    list.files(
+      path_to_indiv_folder
+    ) %>%
+    get_clean_name()
+
+  RUtilpol::output_comment(
+    paste(
+      "Compiling sequences together"
+    )
+  )
+
+  result_list <-
+    subset(
+      ds_present,
+      ds_present %in% ds_vector
+    ) %>%
+    rlang::set_names() %>%
+    purrr::map(
+      .f = ~ RUtilpol::get_latest_file(
+        file_name = .x,
+        dir = path_to_indiv_folder,
+        verbose = FALSE
+      )
+    )
+
+  # save the final list as 'neotoma_download' object filtering out
+  # unsuccessfull sequences
+  neotoma_download <-
+    result_list[!purrr::map_lgl(result_list, is.null)]
+
+  return(neotoma_download)
+}
--- R/proc_neo_get_all_neotoma_datasets.R
+++ R/proc_neo_get_all_neotoma_datasets.R
@@ -12,97 +12,98 @@
 #' datasets_id. This allow aditional possibilities of selection (e.g. polygon).
 #' @seealso [neotoma2::get_sites()]
 #' @export
-proc_neo_get_all_neotoma_datasets <-
-  function(dataset_type,
-           long_min,
-           long_max,
-           lat_min,
-           lat_max,
-           loc) {
-    current_frame <- sys.nframe()
-    current_env <- sys.frame(which = current_frame)
+proc_neo_get_all_neotoma_datasets <- function(dataset_type,
+                                              long_min,
+                                              long_max,
+                                              lat_min,
+                                              lat_max,
+                                              loc) {
+  current_frame <- sys.nframe()
+  current_env <- sys.frame(which = current_frame)
 
-    RUtilpol::check_class("dataset_type", "character")
+  RUtilpol::check_class("dataset_type", "character")
 
-    # if `loc` is not specified by the user, use the traditional geo box
-    if (
-      is.null(loc) == TRUE
-    ) {
-      RUtilpol::check_class("long_min", "numeric")
+  # if `loc` is not specified by the user, use the traditional geo box
+  if (
+    is.null(loc) == TRUE
+  ) {
+    RUtilpol::check_class("long_min", "numeric")
 
-      RUtilpol::check_class("long_max", "numeric")
+    RUtilpol::check_class("long_max", "numeric")
 
-      RUtilpol::check_class("lat_min", "numeric")
+    RUtilpol::check_class("lat_min", "numeric")
 
-      RUtilpol::check_class("lat_max", "numeric")
+    RUtilpol::check_class("lat_max", "numeric")
 
-      loc <-
-        c(
-          long_min,
-          lat_min,
-          long_max,
-          lat_max
-        )
-    }
-
-    # use neotoma2 to get the selected sites
-    sel_sites <-
-      neotoma2::get_sites(loc = geo_box)
-
-    # Transform into a data.frame
-    sel_sites_df <-
-      as.data.frame(sel_sites) %>%
-      dplyr::rename(
-        altitude = elev
-      )
-
-    # Filter all sequences by the geographical limits
-    sel_sites_filtered <-
-      proc_filter_by_geography(
-        sel_sites_df,
+    loc <-
+      c(
         long_min,
-        long_max,
         lat_min,
+        long_max,
         lat_max
       )
-
-    RUtilpol::check_if_loaded(
-      file_name = "sel_sites_filtered",
-      env = current_env
-    )
-
-    RUtilpol::check_class("sel_sites_filtered", "data.frame")
-
-    RUtilpol::output_comment("List of Neotoma sites was successfully obtained.")
-
-    sel_datasets <-
-      neotoma2::get_datasets(
-        sel_sites, # use the ofirginal list od sites
-        all_data = TRUE,
-        verbose = FALSE
-      ) %>%
-      # filter to only include those seletced by altitude
-      neotoma2::filter(siteid %in% sel_sites_filtered$siteid) %>%
-      # filter to only include 
-      neotoma2::filter(datasettype %in% dataset_type)  %>% 
-      # turn into datasets
-      neotoma2::datasets() %>%
-      as.data.frame()  %>% 
-      # rename the column for back compatibility
-      dplyr::rename(
-        dsid = datasetid
-      )
-
-    RUtilpol::check_if_loaded(
-      file_name = "sel_datasets",
-      env = current_env
-    )
-
-    RUtilpol::check_class("sel_datasets", "data.frame")
-
-    RUtilpol::output_comment("List of Neotoma sequences was successfully obtained.")
-
-    util_check_data_table(sel_datasets)
-
-    return(sel_datasets)
   }
+
+  # use neotoma2 to get the selected sites
+  sel_sites <-
+    neotoma2::get_sites(loc = loc)
+
+  # Transform into a data.frame
+  sel_sites_df <-
+    neotoma2:::as.data.frame(sel_sites) %>%
+    dplyr::rename(
+      altitude = elev
+    )
+
+  # Filter all sequences by the geographical limits
+  sel_sites_filtered <-
+    proc_filter_by_geography(
+      sel_sites_df,
+      long_min,
+      long_max,
+      lat_min,
+      lat_max
+    )
+
+  RUtilpol::check_if_loaded(
+    file_name = "sel_sites_filtered",
+    env = current_env
+  )
+
+  RUtilpol::check_class("sel_sites_filtered", "data.frame")
+
+  RUtilpol::output_comment("List of Neotoma sites was successfully obtained.")
+
+  sel_datasets <-
+    neotoma2::get_datasets(
+      sel_sites, # use the ofirginal list od sites
+      all_data = TRUE,
+      verbose = FALSE
+    ) %>%
+    # filter to only include those seletced by altitude
+    neotoma2::filter(siteid %in% sel_sites_filtered$siteid) %>%
+    # filter to only include
+    neotoma2::filter(datasettype %in% dataset_type) %>%
+    # turn into datasets
+    neotoma2::datasets() %>%
+    neotoma2:::as.data.frame() %>%
+    # rename the column for back compatibility
+    dplyr::rename(
+      dsid = datasetid
+    )
+
+  RUtilpol::check_if_loaded(
+    file_name = "sel_datasets",
+    env = current_env
+  )
+
+  RUtilpol::check_class("sel_datasets", "data.frame")
+
+  RUtilpol::output_comment(
+    "List of Neotoma sequences was successfully obtained."
+  )
+
+  util_check_data_table(sel_datasets)
+
+  return(sel_datasets)
+}
