44874a5dcc47e214f7e1f51a252b967371fd9dc4
Ondrej Mottl <ondrej.mottl@gmail.com>
2023-03-16 19:55:21 +0100

    proc_neo_extract_samples: fix issue with nested list

--- R/proc_neo_extract_samples.R
+++ R/proc_neo_extract_samples.R
@@ -5,89 +5,104 @@
 #' and nested table with all the sample info.
 #' @return Data.frame for each of the datasets with nested sample data
 #' @keywords internal
-proc_neo_extract_samples <-
-  function(data_source,
-           sel_dataset_id) {
-    current_frame <- sys.nframe()
-    current_env <- sys.frame(which = current_frame)
+proc_neo_extract_samples <- function(data_source,
+                                     sel_dataset_id) {
+  current_frame <- sys.nframe()
+  current_env <- sys.frame(which = current_frame)
 
-    RUtilpol::check_class("data_source", "list")
+  RUtilpol::check_class("data_source", "list")
 
-    RUtilpol::check_class("sel_dataset_id", "character")
+  RUtilpol::check_class("sel_dataset_id", "character")
 
-    # prepare the table with sample_id and depth
-    try(
-      sample_table <-
-        tibble::tibble(
-          sample_id = data_source %>%
-            purrr::map_chr("sampleid"),
-          depth = data_source %>%
-            purrr::map_dbl("depth")
-        ),
-      silent = TRUE
-    )
-
-    # if the sample table exists and it is longer than 0 rows
-    if (
-      exists("sample_table", envir = current_env)
-    ) {
-      if (
-        nrow(sample_table) > 0
-      ) {
-        # create a list with sample id replicated X times, where x is the number
-        #   of taxa (this is needed for adding the sample id in future)
-        sample_id_list <-
-          data_source %>%
-          purrr::set_names(nm = sample_table$sample_id) %>%
-          purrr::map(
-            "datum",
-            .progress = "Creating a reference list"
-          ) %>%
-          base::summary() %>%
-          as.data.frame() %>%
-          tibble::as_tibble() %>%
-          dplyr::filter(Var2 == "Length") %>%
-          dplyr::mutate(sample_id = Var1) %>%
-          dplyr::select(-c(Var1, Var2)) %>%
-          dplyr::mutate(
-            Freq = purrr::map(
-              .x = Freq,
-              .f = ~ seq(1:.x)
-            )
-          ) %>%
-          tidyr::unnest(cols = c(Freq)) %>%
-          dplyr::select(-Freq)
-
-        # create a nested tibble with one r
-        temp_data <-
-          data_source %>%
-          purrr::map(
-            "datum",
-            .progress = "Transforming records into a table"
-          ) %>%
-          purrr::list_rbind() %>%
-          dplyr::mutate(
-            sample_id = sample_id_list$sample_id
-          ) %>%
-          tidyr::nest(sample_detail = dplyr::any_of(
-            c(
-              "taxonid", "units", "value", "element",
-              "elementtype", "variablename", "taxongroup",
-              "context", "ecologicalgroup"
-            )
-          )) %>%
-          dplyr::inner_join(., sample_table, by = "sample_id") %>%
-          dplyr::select(sample_id, depth, sample_detail) %>%
-          dplyr::mutate(dataset_id = sel_dataset_id) %>%
-          dplyr::distinct(sample_id, depth, .keep_all = TRUE) %>%
-          tidyr::nest(samples = c(sample_id, depth, sample_detail))
-      } else {
-        temp_data <-
-          tibble::tibble(
-            dataset_id = sel_dataset_id,
-            samples = NA
+  # prepare the table with sample_id and depth
+  try(
+    sample_table <-
+      tibble::tibble(
+        sample_id = data_source %>%
+          purrr::map_chr(
+            .f = ~ .x %>%
+              purrr::pluck("sampleid") %>%
+              as.character()
+          ),
+        depth = data_source %>%
+          purrr::map_dbl(
+            .f = ~ .x %>%
+              purrr::pluck("depth" %>%
+                as.character())
           )
-      }
+      ),
+    silent = TRUE
+  )
+
+  # if the sample table exists and it is longer than 0 rows
+  if (
+    exists("sample_table", envir = current_env)
+  ) {
+    if (
+      nrow(sample_table) > 0
+    ) {
+      # create a list with sample id replicated X times, where x is the number
+      #   of taxa (this is needed for adding the sample id in future)
+      sample_id_list <-
+        data_source %>%
+        purrr::set_names(nm = sample_table$sample_id) %>%
+        purrr::map(
+          "datum",
+          .progress = "Creating a reference list"
+        ) %>%
+        base::summary() %>%
+        as.data.frame() %>%
+        tibble::as_tibble() %>%
+        dplyr::filter(Var2 == "Length") %>%
+        dplyr::mutate(sample_id = Var1) %>%
+        dplyr::select(-c(Var1, Var2)) %>%
+        dplyr::mutate(
+          Freq = purrr::map(
+            .x = Freq,
+            .f = ~ seq(1:.x)
+          )
+        ) %>%
+        tidyr::unnest(cols = c(Freq)) %>%
+        dplyr::select(-Freq)
+
+      # create a nested tibble with one r
+      temp_data <-
+        data_source %>%
+        purrr::map(
+          .f = ~ .x %>%
+            purrr::pluck("datum"),
+        ) %>%
+        purrr::list_flatten() %>%
+        purrr::map(
+          .progress = "Transforming records into a table",
+          .f = ~ .x %>%
+            unlist() %>%
+            as.data.frame() %>%
+            tibble::rownames_to_column("name") %>%
+            rlang::set_names(
+              nm = c("name", "value")
+            ) %>%
+            tidyr::pivot_wider(
+              names_from = "name",
+              values_from = "value"
+            )
+        ) %>%
+        purrr::list_rbind() %>%
+        dplyr::mutate(
+          sample_id = sample_id_list$sample_id
+        ) %>%
+        tidyr::nest(sample_detail = dplyr::any_of(
+          c(
+            "taxonid", "units", "value", "element",
+            "elementtype", "variablename", "taxongroup",
+            "context", "ecologicalgroup"
+          )
+        )) %>%
+        dplyr::inner_join(., sample_table, by = "sample_id") %>%
+        dplyr::select(sample_id, depth, sample_detail) %>%
+        dplyr::mutate(dataset_id = sel_dataset_id) %>%
+        dplyr::distinct(sample_id, depth, .keep_all = TRUE) %>%
+        tidyr::nest(samples = c(sample_id, depth, sample_detail))
     } else {
       temp_data <-
         tibble::tibble(
@@ -95,12 +110,19 @@ proc_neo_extract_samples <-
           samples = NA
         )
     }
-
-    if (
-      exists("sample_table", envir = current_env)
-    ) {
-      rm(sample_table, envir = current_env)
-    }
-
-    return(temp_data)
+  } else {
+    temp_data <-
+      tibble::tibble(
+        dataset_id = sel_dataset_id,
+        samples = NA
+      )
   }
+
+  if (
+    exists("sample_table", envir = current_env)
+  ) {
+    rm(sample_table, envir = current_env)
+  }
+
+  return(temp_data)
+}
